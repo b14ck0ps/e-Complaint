d62fc882d5b63ee74d35ee777a291c06dc22775f
Azran Hossain <mr69751@gmail.com>
2022-12-24 16:52:21 +0600

    type Upcase

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('type')->default('victim');
+            $table->string('type')->default('VICTIM');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
