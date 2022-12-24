607b38f0758c2c918789c25aac6d5cde37cce8fa
Azran Hossain <mr69751@gmail.com>
2022-12-24 20:50:32 +0600

    2more column added #noplandb -_-

--- database/migrations/2022_12_11_092557_create_complains_table.php
+++ database/migrations/2022_12_11_092557_create_complains_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->text('description');
             $table->string('status')->default('pending'); // pending, in progress, resolved
             $table->string('anonymous')->default(0); // 0 = false, 1 = true
+            $table->string('handle_by')->nullable(); // cyber police, hq
+            $table->string('assign_to')->nullable(); // cyber police, hq
             //user table foreign key
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
