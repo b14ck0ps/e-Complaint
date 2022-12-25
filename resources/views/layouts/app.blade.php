5abb21c1bbab59bed3420689e734357046b7fec4
Azran Hossain <mr69751@gmail.com>
2022-12-25 08:15:48 +0600

    vite off

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     {{-- fontawesome --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    @vite(['/resources/css/app.css'])
+    {{-- @vite(['/resources/css/app.css']) --}}
 </head>
 
 <body>
